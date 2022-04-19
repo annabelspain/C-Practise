8f1b4c4884e5f2f76314f483d16331161bd97a77
Annabel <72205175+annabelspain@users.noreply.github.com>
2022-04-19 17:14:52 +0100

    Add files via upload

---
+++ End/PizzaProj/Checkout/OrderTooBigException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaProj
+{
+    public class OrderTooBigException : Exception
+    {
+        public OrderTooBigException(string msg) : base(msg)
+        {
+        }
+    }
+}
