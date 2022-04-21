56fa4f734a7a106e6a94a47f0b21ef8ee0852d5c
Annabel <72205175+annabelspain@users.noreply.github.com>
2022-04-21 13:49:56 +0100

    Add files via upload

--- 16  Entity Framework/Begin/EF/EF/Animal.cs
+++ 16  Entity Framework/Begin/EF/EF/Animal.cs
@@ -6,7 +6,9 @@ namespace EF
 {
     public class Animal
     {
+        public int AnimalId { get; set; }
         public string Type { get; set; }
         public string Name { get; set; }
+        public virtual Zoo ZooId { get; set; }
     }
 }
---
+++ 16  Entity Framework/Begin/EF/EF/bin/Debug/netcoreapp3.1/Program.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace EF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool createNewDatabase = false;
+            ServiceProvider serviceProvider = ConfigureServices();
+            ZooContext ctx = serviceProvider.GetService<ZooContext>();
+            if (createNewDatabase)
+            {
+                ctx.Database.EnsureDeleted();
+                ctx.Database.EnsureCreated();
+                LoadZoo(ctx);
+            }
+            else
+            {
+                ViewZoo(ctx);
+            }
+        }
+
+        static void ViewZoo(ZooContext ctx)
+        {
+            foreach (Zoo zoo in ctx.Zoos /*.Include(z => z.Animals)*/ )
+            {
+                //ctx.Entry(zoo).Collection(z => z.Animals).Load();
+                Console.WriteLine($"\nName = {zoo.Name}, number of animals ={ zoo.Animals.Count}");
+            foreach (Animal animal in zoo.Animals)
+                {
+                    Console.WriteLine($"...{animal.Type,-10}{animal.Name}");
+                }
+            }
+        }
+
+        static void LoadZoo(ZooContext ctx)
+        {
+            List<Zoo> zoos = new List<Zoo>();
+            Zoo londonZoo = new Zoo() { Name = "London" };
+            Zoo edinburghZoo = new Zoo() { Name = "Edinburgh" };
+            zoos.Add(londonZoo);
+            zoos.Add(edinburghZoo);
+            londonZoo.Animals.Add(new Animal() { Type = "Elephant", Name = "Dumbo" });
+            londonZoo.Animals.Add(new Animal() { Type = "Elephant", Name = "Heffalumps" });
+            londonZoo.Animals.Add(new Animal() { Type = "Lion", Name = "Clarence" }); // Lion tamer = Claude Bottom
+            edinburghZoo.Animals.Add(new Animal() { Type = "Panda", Name = "Sweetie" });
+            edinburghZoo.Animals.Add(new Animal() { Type = "Panda", Name = "Sunshine" });
+
+            foreach (Zoo zoo in zoos)
+            {
+                Console.WriteLine($"\nName = {zoo.Name}, number of animals = {zoo.Animals.Count}");
+                foreach (Animal animal in zoo.Animals)
+                {
+                    Console.WriteLine($"...{animal.Type,-10}{animal.Name}");
+                }
+            }
+            // write to database
+            foreach (Zoo zoo in zoos)
+            {
+                ctx.Zoos.Add(zoo);
+            }
+            ctx.SaveChanges();
+        }
+
+        private static ServiceProvider ConfigureServices()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange:true);
+
+            IConfigurationRoot configuration = builder.Build();
+            string strConn = configuration.GetConnectionString("conn");
+            Console.WriteLine(configuration.GetConnectionString("conn"));
+
+            var services = new ServiceCollection();
+            services.AddDbContext<ZooContext>
+                (options => options.UseSqlServer(strConn));
+
+            var serviceProvider = services.BuildServiceProvider();
+            return serviceProvider;
+        }
+
+    }
+}
